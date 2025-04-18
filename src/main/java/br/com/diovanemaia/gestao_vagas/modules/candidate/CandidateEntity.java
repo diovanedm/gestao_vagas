ba13710b6a5512cc5cf10843d28c17d8ef8632ae
Diovane Maia <diovane@saudeagora.com.vc>
2025-04-18 10:34:50 -0300

    criando repositório

--- src/main/java/br/com/diovanemaia/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/diovanemaia/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,24 @@
 package br.com.diovanemaia.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
 
@@ -21,7 +30,10 @@ public class CandidateEntity {
 
   @Length(min = 10, max = 30, message = "O campo [senha] deve ter entre (10) e (30) caracteres")
   private String password;
+
   private String description;
   private String curriculum;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
